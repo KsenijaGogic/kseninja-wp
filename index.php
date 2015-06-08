b9c0e46ffa508354ca457ff4b9c182432e66acee
Ksenija Gogic <ksenijagogic@gmail.com>
2015-06-08 10:40:37 -0400

    Loads of changes.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php get_header(); ?>
 
 <main>
+<!-- SIDEBARS START -->
 	<div class="sidebar sidebarFull">
 		<aside class="first animated fadeIn">
 			<h1 class="name">Ksenija Gogic</h1>
@@ -9,15 +10,18 @@
 		</aside>
 		
 		<aside class="second animated fadeIn">
-			<h1 class="name">Ksenija Gogic</h1>
-			<h3 class="tagline">Designer & Developer</h3>
+			<div class="nameTitle">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/helloHeaderNew-01.png" class="header" alt="Header image.">
+				<h1 class="name">Ksenija Gogic</h1>
+				<h3 class="tagline">Designer & Developer</h3>
+			</div>
 			<ul class="nav sidebarNav">
 				<li><a href="#dev">Development</a></li>
 				<li><a href="#design">Design</a></li>
 				<li><a href="#contact">Contact</a></li>
 				<li><a href="#top">Top</a></li>
 			</ul>
-			<ul class="social">
+			<ul class="social socialSidebar">
 				<li><a href="https://ca.linkedin.com/in/ksenijagogic"><i class="fa fa-linkedin"></i></a></li>
 				<li><a href="mailto:k@kse.ninja?Subject=Hi!" target="_top"><i class="fa fa-envelope"></i></a></li>
 				<li><a href="https://medium.com/@kseninja"><i class="fa fa-medium"></i></a></li>
@@ -25,8 +29,10 @@
 			</ul>
 		</aside>
 	</div>
-			
-	<section class="content contentShrink">
+<!-- SIDEBARS END -->
+		
+<!-- CONTENT START -->			
+	<section class="content contentShrink animated fadeIn">
 		<div class="intro hunna" id="top">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/helloHeader-01.png" class="header" alt="Header image.">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium libero necessitatibus odio fuga distinctio reiciendis, mollitia maiores omnis odit, labore neque quasi at. Maxime, autem velit quae nostrum earum nemo.</p>
@@ -35,73 +41,78 @@
 				<li><a href="#design">Design</a></li>
 				<li><a href="">Contact</a></li>
 			</ul>
-			<ul class="social">
+			<ul class="social socialIntro">
 				<li><a href="https://ca.linkedin.com/in/ksenijagogic"><i class="fa fa-linkedin"></i></a></li>
 				<li><a href="mailto:k@kse.ninja?Subject=Hi!" target="_top"><i class="fa fa-envelope"></i></a></li>
 				<li><a href="https://medium.com/@kseninja"><i class="fa fa-medium"></i></a></li>
 				<li><a href="https://twitter.com/kseninja"><i class="fa fa-twitter"></i></a></li>
 			</ul>
 		</div>
-			
-		<section class="dev" id="dev">
-			<h2>Web Development</h2>			
-			<?php
-			$onePageQuery = new WP_Query(
-				array(
-					'posts_per_page' => -1,
-					'post_type' => 'portfolio',
-					'order' => 'ASC'
-					)
-			); ?>
-			
-			<?php if ( $onePageQuery->have_posts() ) : ?>
-			
-			<?php while ($onePageQuery->have_posts()) : $onePageQuery->the_post(); ?>
-			
-			<section id="<?php echo $post->post_name; ?>">
-				<h2><?php the_title(); ?></h2>
-				<?php the_post_thumbnail(); ?>
-				<div class="images">
-					<?php while( has_sub_field('images') ): ?>
-					<?php endwhile; ?>
-					<p><?php the_field('short_desc') ?></p>
-				</div>
-			</section>
-			<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
-		</section><?php endif; ?>
-
-		<section class="design" id="design">
-			<h2>Design</h2>			
-			<?php
-			$onePageQuery = new WP_Query(
-				array(
-					'posts_per_page' => -1,
-					'post_type' => 'design',
-					'order' => 'ASC'
-					)
-			); ?>
-			
-			<?php if ( $onePageQuery->have_posts() ) : ?>
-			
-			<?php while ($onePageQuery->have_posts()) : $onePageQuery->the_post(); ?>
-			
-			<section id="<?php echo $post->post_name; ?>">
-				<h2><?php the_title(); ?></h2>
-				<?php the_post_thumbnail(); ?>
-				<div class="images">
-					<?php while( has_sub_field('images') ): ?>
-					<div class="image">
-						<p><?php the_sub_field('caption'); ?></p>
+<!-- DEV PROJECTS START -->
+		<section class="dev project animated fadeIn" id="dev">
+			<h2 class="sectionTitle">Web Development</h2>			
+			<div class="projectGallery">
+				<?php
+				$onePageQuery = new WP_Query(
+					array(
+						'posts_per_page' => -1,
+						'post_type' => 'portfolio',
+						'order' => 'ASC'
+						)
+				); ?>
+				
+				<?php if ( $onePageQuery->have_posts() ) : ?>
+				
+				<?php while ($onePageQuery->have_posts()) : $onePageQuery->the_post(); ?>
+				
+				<section class="projectEach"id="<?php echo $post->post_name; ?>">
+					<?php the_post_thumbnail(); ?>
+					<div class="images">
+						<?php while( has_sub_field('images') ): ?>
+						<?php endwhile; ?>
+						<h2 class="projectTitle"><?php the_title(); ?></h2>
+						<p><?php the_field('short_desc') ?></p>
 					</div>
-					<?php endwhile; ?>
-				</div>
-			</section>
-			<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
+				</section>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+			</div>
 		</section><?php endif; ?>
-			
+<!-- DEV PROJECTS END -->
+
+<!-- DESIGN PROJECTS START -->
+		<section class="design project animated fadeIn" id="design">
+			<h2 class="sectionTitle">Design</h2>			
+			<div class="projectGallery"><?php
+				$onePageQuery = new WP_Query(
+					array(
+						'posts_per_page' => -1,
+						'post_type' => 'design',
+						'order' => 'ASC'
+						)
+				); ?>
+				
+				<?php if ( $onePageQuery->have_posts() ) : ?>
+				
+				<?php while ($onePageQuery->have_posts()) : $onePageQuery->the_post(); ?>
+				
+				<section id="<?php echo $post->post_name; ?>">
+					<?php the_post_thumbnail(); ?>
+					<div class="images">
+						<?php while( has_sub_field('images') ): ?>
+						<?php endwhile; ?>
+						<h2 class="projectTitle"><?php the_title(); ?></h2>
+						<p><?php the_field('short_desc') ?></p>
+					</div>
+				</section>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+			</div>
+		</section><?php endif; ?>
+<!-- DESIGN PROJECTS END -->
+
 	</section> <!-- .content  -->
+<!-- CONTENT END -->			
 </main>
 
 
